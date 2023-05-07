55ac69346a3f2e4a4655ac0c83e71acbe871296f
genkovich <genkovich@syneforge.com>
2023-05-07 09:04:32 +0300

    php-cs fixer rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,18 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@Symfony' => true,
+        '@PHP82Migration' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
     ])
     ->setFinder($finder)
 ;
